57cdfe48982b66a9eb589cf5e04543afccc42140
vysf <yusufumar@student.untan.ac.id>
2022-02-08 17:00:15 +0700

    tambahan file index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
-// import "./css/index.css";
+// import "./index.scss";
+import "./index.css";
 // import App from "./pages/App";
 import reportWebVitals from "./reportWebVitals";
 import App from "./pages/App";
